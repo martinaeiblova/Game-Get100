5f099a546992f3f9f0ded157c4222b30461afc52
Martina Eiblová <emartina@seznam.cz>
2022-10-09 23:26:21 +0200

    Variables defined

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-'use strict'
+"use strict";
+
 
 
 const diceEl = document.querySelector(".dice");
-
+const btnNew = document.querySelector(".btn-new-game");
+const btnRoll = document.querySelector(".btn-roll-dice");
+const btnHold = document.querySelector(".btn-hold");
+const score1El = document.querySelector("#score--1");
+const score2El = document.querySelector("#score--2");
 
 diceEl.classList.add("hidden");
+
+
+
+
 

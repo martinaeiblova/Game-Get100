0229f7817de4193765a16c99d28ce553a2168509
Martina Eiblová <emartina@seznam.cz>
2022-10-19 14:15:11 +0200

    Switch player and clear current score

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 "use strict";
 
-
-
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn-new-game");
 const btnRoll = document.querySelector(".btn-roll-dice");
@@ -12,30 +10,26 @@ const currentScore0 = document.querySelector("#current--0");
 const currentScore1 = document.querySelector("#current--1");
 
 diceEl.classList.add("hidden");
-
-const scores = [0,0];
 let currentScore = 0;
+const scores = [0, 0];
 let activePlayer = 0;
-
 
 // click btnRoll: generate random number, displey this number
 // když je 1 - switch player
 // když jiné číslo - add dice roll (randNum) to current score
 //                 - display new score
 
-
-btnRoll.addEventListener("click", function() {
-    const randNum = Math.trunc(Math.random()*6 ) + 1;
+btnRoll.addEventListener("click", function () {
+    const randNum = Math.trunc(Math.random() * 6) + 1;
     diceEl.classList.remove("hidden");
-    diceEl.src =`dice-${randNum}.png`;
-    if (randNum != 1){
+    diceEl.src = `dice-${randNum}.png`;
+    if (randNum != 1) {
         currentScore += randNum;
-        document.getElementById("current--${activePlayer}").textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent =
+            currentScore;
+    } else {
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
     }
-    else {
-        
-
-    }
-})
-
-
+});
